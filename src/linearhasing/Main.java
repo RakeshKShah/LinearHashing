f300e76b0b898e4d6ff8a4c0ab68495d1813d4f3
Rakesh Kumar Shah <rakesh.shah.iiita@gmail.com>
2017-04-19 02:27:54 -0500

    Create Storage for Read, Write,Allocate and Deallocate Page

--- src/linearhasing/Main.java
+++ src/linearhasing/Main.java
@@ -2,7 +2,7 @@ package linearhasing;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        CreateStorage s1 = new CreateStorage("myDisk1", 1024, 1024*100);
     }
 }
